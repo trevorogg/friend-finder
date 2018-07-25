85999fae87eaa33328750483e2272acabe72c380
trevorogg <ogg.trevor@gmail.com>
2018-07-24 23:13:49 -0600

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,31 +21,23 @@ router.post("/api/friends", function (req, res) {
 
     let friendName = "";
     let friendImg = "";
-    //establishes a value that the resulting sum will always be less than, to get into if statement
+
     let total = 500;
 
 
     for (let i = 0; i < users.surveyResults.length; i++) {
         let diff = 0;
-        //new array with the difference values
         let difference = [];
 
         for (let j = 0; j < answers.length; j++) {
-            //takes absolute value of differences
             let diff = Math.abs(answers[j] - users.surveyResults[i].scores[j]);
-            //pushes difference to new array
             difference.push(diff);
-
         }
         console.log(difference);
-        //adds up the sum of all values in the difference array
         let sum = difference.reduce(function (acc, val) { return acc + val; });
         console.log("sum is " + sum)
-        //checks each friend against a total, and the lowest total difference will be the resulting friend
         if (sum < total) {
-            //establishes the new total difference to beat
             total = sum;
-            //resets the sum each loop
             sum = 0;
             friendName = users.surveyResults[i].name;
             friendImg = users.surveyResults[i].image;
